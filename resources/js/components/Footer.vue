005ad952d3990cecffcc7da8f057456bdb7fbcad
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-23 17:51:17 +0100

    Add header,main,footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,3 +1,11 @@
+<template>
+  <footer>
+      <h1>Ciao sono il footer piu brutto di sempre</h1>
+  </footer>
+</template>
 
-
-
+<script>
+export default {
+ name: 'Footer'
+}
+</script>
--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,3 +1,13 @@
+<template>
+  <main>
+      <h1>Ciao sono il main piu brutto di sempre</h1>
+  </main>
+</template>
 
+<script>
+export default {
+ name: 'Main'
+}
+</script>
 
 
