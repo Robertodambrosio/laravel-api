31daf114aee78f056fecb900fcfb88d85d133228
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-23 16:56:31 +0100

    creation App.vue + components

--- resources/js/front.js
+++ resources/js/front.js
@@ -3,8 +3,9 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+window.axios = require('axios');
 
-require('./bootstrap');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Vue = require('vue');
 
@@ -19,14 +20,15 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import App from './App.vue';
+
 const app = new Vue({
     el: '#app',
+    render: h => h(App),
 });
